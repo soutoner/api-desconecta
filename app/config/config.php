69176a4d5cdfde8029dafbfebdb7e3cf1e2b1153
Adrian Gonzalez <adrigonle@gmail.com>
2015-12-04 17:18:44 +0100

    Initial commit 2

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,23 @@
 <?php
 
-$config = new \Phalcon\Config(array(
-    "database" => array(
-        "adapter" => "Mysql",
-        "host" => "localhost",
-        "username" => "root",
-        "password" => "",
-        "dbname" => "desconecta_dev"
+defined('APP_PATH') || define('APP_PATH', realpath('.'));
+
+return new \Phalcon\Config(array(
+    'database' => array(
+        'adapter'     => 'Mysql',
+        'host'        => 'localhost',
+        'username'    => 'root',
+        'password'    => '',
+        'dbname'      => 'desconecta_dev',
+        'charset'     => 'utf8',
     ),
-    "phalcon" => array(
-        "modelsDir" => "../models/"
+    'application' => array(
+        'controllersDir' => APP_PATH . '/app/controllers/',
+        'modelsDir'      => APP_PATH . '/app/models/',
+        'migrationsDir'  => APP_PATH . '/app/migrations/',
+        'pluginsDir'     => APP_PATH . '/app/plugins/',
+        'libraryDir'     => APP_PATH . '/app/library/',
+        'cacheDir'       => APP_PATH . '/app/cache/',
+        'baseUri'        => '/foo/',
     )
 ));
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Phalcon\Mvc\Micro;
-
-$app = new Micro();
-
-// Define the routes here
-
-$app->handle();
