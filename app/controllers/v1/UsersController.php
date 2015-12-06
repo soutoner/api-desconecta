f7d0f7df51b3061bceb7f6c0703e37e8255b4ce4
Adrian Gonzalez <adrigonle@gmail.com>
2015-12-06 12:26:39 +0100

    Update using PUT

--- app/controllers/v1/UsersController.php
+++ app/controllers/v1/UsersController.php
@@ -5,7 +5,11 @@ namespace v1;
 
 class UsersController extends \ControllerBase
 {
-
+    /**
+     * Returns all the users in the database.
+     *
+     * TODO: Pagination
+     */
     public function index()
     {
         $users = $this->modelsManager->createBuilder()
@@ -30,9 +34,15 @@ class UsersController extends \ControllerBase
         echo json_encode($data);
     }
 
+    /**
+     * Creates an user in the database.
+     *
+     * TODO: Create custom filters (e.g. filter for dates)
+     *
+     * @return \Phalcon\Http\Response
+     */
     public function create()
     {
-        // TODO: Create custom filters
         $request = $this->request;
 
         $phql = "INSERT INTO User (name, surname, email, profile_picture, date_birth, gender, location)
@@ -51,6 +61,12 @@ class UsersController extends \ControllerBase
         return $this->response($request, $status);
     }
 
+    /**
+     * Updates an user. Always use `x-www-form-urlencoded` content type for PUT.
+     *
+     * @param $id - Id of the user to be deleted
+     * @return \Phalcon\Http\Response
+     */
     public function update($id){
         $request = $this->request;
 
@@ -67,18 +83,24 @@ class UsersController extends \ControllerBase
           WHERE id = :id:";
         $status = $this->modelsManager->executeQuery($phql, [
             'id'                => $id,
-            'name'              => $request->getPost('name', 'string', $user->name),
-            'surname'           => $request->getPost('surname', 'string', $user->surname),
-            'email'             => $request->getPost('email', 'email', $user->email),
-            'profile_picture'   => $request->getPost('profile_picture', 'string', $user->profile_picture),
-            'date_birth'        => $request->getPost('date_birth', 'string', $user->date_birth),
-            'gender'            => $request->getPost('gender', 'string', $user->gender),
-            'location'          => $request->getPost('location', 'string', $user->location),
+            'name'              => $request->getPut('name', 'string', $user->name),
+            'surname'           => $request->getPut('surname', 'string', $user->surname),
+            'email'             => $request->getPut('email', 'email', $user->email),
+            'profile_picture'   => $request->getPut('profile_picture', 'string', $user->profile_picture),
+            'date_birth'        => $request->getPut('date_birth', 'string', $user->date_birth),
+            'gender'            => $request->getPut('gender', 'string', $user->gender),
+            'location'          => $request->getPut('location', 'string', $user->location),
         ]);
 
         return $this->response($request, $status);
     }
 
+    /**
+     * Deletes an user from the database.
+     *
+     * @param $id - Id of the user to be deleted
+     * @return \Phalcon\Http\Response
+     */
     public function delete($id)
     {
         $phql = "DELETE FROM User WHERE id = :id:";
@@ -89,6 +111,5 @@ class UsersController extends \ControllerBase
 
         return $this->response($this->request ,$status);
     }
-
 }
 
