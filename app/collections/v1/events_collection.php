9623eb83309caf7cd32a959c07935e2aebecca21
lotape6 <lotape6@uma.es>
2015-12-09 22:55:01 +0100

    Re-created event model, controller and collection

---
+++ app/collections/v1/events_collection.php
@@ -0,0 +1,24 @@
+<?php
+
+
+use \Phalcon\Mvc\Micro\Collection as MicroCollection;
+
+$version = basename(dirname(__FILE__));
+$controller_path = 'App\Controllers\\' . strtoupper($version) . '\\';
+
+/**
+ * Setup collection
+ */
+$events = new MicroCollection();
+$events->setHandler($controller_path . 'EventsController', true);
+$events->setPrefix('/api/'. $version .'/events');
+
+/**
+ * Define routes
+ */
+$events->get('/', 'index');
+$events->post('/', 'create');
+$events->put('/{id:[0-9]+}', 'update');
+$events->delete('/{id:[0-9]+}', 'delete');
+
+return $events;
